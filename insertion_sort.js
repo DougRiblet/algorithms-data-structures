b9d06cf10b3aee15c5a3083466cec7082ea4e037
Doug Riblet <smashzen85@gmail.com>
2017-07-29 18:14:47 -0500

    insertion sort with comparator

--- insertion_sort.js
+++ insertion_sort.js
@@ -14,7 +14,7 @@
 
 // BASIC VERSION
 
-function insertionSort (input) {
+function insertionSort(input) {
   let curr = 1;
   while(curr < input.length){
     let c = curr;
@@ -32,4 +32,36 @@ function insertionSort (input) {
   }
   return input;
 }
+// Time complexity for insertionSort: quadratic ... O(n^2)
+// For nearly sorted arrays, time complexity approaches linear ... O(n)
 
+// COMPARATOR VERSION
+
+function insertionSortWithComparator (input, comparator) {
+  const defaultComp = (a,b) => a>b;
+  let comp = comparator || defaultComp;
+  let curr = 1;
+  while(curr < input.length){
+    let c = curr;
+    for (let j=curr-1; j>=0; j--){
+      if (comp(input[j], input[c])){
+        let temp = input[j];
+        input[j] = input[c];
+        input[c] = temp;
+        c--;
+      } else {
+        break;
+      }
+    }
+    curr++;
+  }
+  return input;
+}
+
+// Testing Stability with Comparator Version
+
+let arrOfObj = [{value: 15}, {value: 10, order: 1}, {value: 10, order: 2}];
+
+const valcomp = (a,b) => a.value > b.value;
+
+insertionSortWithComparator(arrOfObj, valcomp);
