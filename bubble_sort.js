25f8cd10a186df73bd275178d432a2ffc9af4e7b
Doug Riblet <smashzen85@gmail.com>
2017-07-29 15:49:17 -0500

    add insertion sort

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,1 +1,39 @@
-bubble_sort.js
+
+//https://github.com/kuychaco/algoClass/blob/master/sorting-algorithms/bubble.js
+// *** Description
+// Iterate over array, comparing adjacent items and swap if in incorrect order.
+// Largest elements bubble to the end of the array
+// *** Exercises
+// - Implement bubble sort
+// - Identify time complexity
+// *** Optimizations:
+// - Make algorithm adaptive (if at any point array is already sorted, 
+// exit function early). After doing this, what is time complexity 
+// for nearly sorted arrays?
+// - For each pass through the array, are you doing 
+// any unnecessary checking of elements? Minimize checking 
+// and consider the effect on time complexity.
+
+function bubbleSort (input) {
+  let cutoff = input.length-1;
+  while (cutoff > 0) {
+    let newco = 0;
+    for (let x=0; x<cutoff; x++) {
+      if (input[x] > input[x+1]) {
+        let temp = input[x];
+        input[x] = input[x+1];
+        input[x+1] = temp;
+        newco = x;
+      }
+    }
+    cutoff = newco;
+  }
+  return input;
+}
+// Time complexity for bubble sort: quadratic ... O(n^2)
+
+// Optimizations:
+// 1. Each pass stops checking at index of final swap of previous pass
+// 2. If a pass registers zero swaps, while loop ends and function returns
+// Time complexity for nearly sorted arrays approaches linear ... O(n)
+
---
+++ insertion_sort.js
@@ -0,0 +1,38 @@
+
+// https://github.com/kuychaco/algoClass/blob/master/sorting-algorithms/insertion.js
+// *** Description
+// Iterate over array and grow a sorted array behind current element.
+// For each position, compare value of element with previous elements
+// and swap positions if previous element is larger.
+// *** Exercises
+// - Implement insertion sort for array of numbers
+// - Identify time complexity
+// - Modify function to take comparator function. specify default if not 
+//   provided (check out native Array.sort comparator function for reference)
+// - Use your comparator function to verify that your sort is stable by 
+//   taking input: [{value: 15}, {value: 10, order: 1}, {value: 10, order: 2}]
+
+// BASIC VERSION
+
+function insertionSort (input) {
+  let curr = 1;
+
+  while(curr < input.length){
+    console.log(String(curr), ": ", input);
+    let c = curr;
+    for (let j=curr-1; j>=0; j--){
+      if (input[j] > input[c]){
+        console.log("swap: ", input[j], input[c]);
+        let temp = input[j];
+        input[j] = input[c];
+        input[c] = temp;
+        c--;
+      } else {
+        break;
+      }
+    }
+    curr++;
+  }
+  return input;
+}
+
