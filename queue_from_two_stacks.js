889d8ad6214822df24edd8f9cd5a992f8cb7d982
Doug Riblet <smashzen85@gmail.com>
2017-07-30 12:26:46 -0500

    finish queue from two stacks

--- queue_from_two_stacks.js
+++ queue_from_two_stacks.js
@@ -1,21 +1,53 @@
 // https://github.com/kuychaco/algoClass/blob/master/data-structures/queue.js
 // BONUS EXERCISE: Implement a queue using two stacks.
 
-var Stack = function() {
-  var storage = [];
-  this.push = function(v){ storage.push(v) };
-  this.pop = function(){ return storage.pop() };
-  this.size = function(){ return storage.length };
+// First, set up Stack constructor and methods
+
+function Stack() {
+  this.storage = {};
+  this.topmost = 0;
+}
+
+Stack.prototype.push = function(value) {
+  this.storage[String(this.topmost)] = value;
+  this.topmost++;
+  return this.topmost;
 };
 
-var Queue = function() {
-  var inbox = new Stack();
-  var outbox = new Stack();
-  this.enqueue = function(v) {
-    while(outbox.size()) { inbox.push(outbox.pop()) }
-    outbox.push(v);
-    while(inbox.size()) { outbox.push(inbox.pop()) }
-  };
-  this.dequeue = function(){ return outbox.pop() };
-  this.size = function(){ return outbox.size() };
+Stack.prototype.pop = function() {
+  if (this.topmost > 0) {
+    let popItem = this.storage[String(this.topmost - 1)];
+    delete this.storage[String(this.topmost - 1)];
+    this.topmost--;
+    return popItem;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.topmost;
+};
+
+// Now, set up Queue constructor using two stacks
+
+function Queue() {
+  this.inbox = new Stack();
+  this.outbox = new Stack();
+};
+
+Queue.prototype.enqueue = function(value) {
+  while(this.outbox.size()) {
+    this.inbox.push(this.outbox.pop());
+  }
+  this.outbox.push(value);
+  while(this.inbox.size()) {
+    this.outbox.push(this.inbox.pop());
+  }
+};
+
+Queue.prototype.dequeue = function(){
+  return this.outbox.pop();
+};
+
+Queue.prototype.size = function(){
+  return this.outbox.size();
 };
