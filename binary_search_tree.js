d69e6975be3344e56ef4586fa9674cdc89f98630
Doug Riblet <smashzen85@gmail.com>
2017-08-01 18:35:06 -0500

    add first 2 exercises

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -9,4 +9,22 @@
 // - all the values in the right subtree of a node 
 //     are greater than the value of the node
 
+// Constructor function provided
 
+function BinarySearchTree (value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+// EXERCISES: Implement these methods and note time complexity for each
+
+BinarySearchTree.prototype.insert = function(value) {
+  // implement me...
+};
+// Time complexity:
+
+BinarySearchTree.prototype.contains = function(value) {
+  // implement me...
+};
+// Time complexity:
