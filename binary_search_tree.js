eef46b98a910eed9d8d464cc017db46a648283ed
Doug Riblet <smashzen85@gmail.com>
2017-08-01 18:34:03 -0500

    add binary search tree

---
+++ binary_search_tree.js
@@ -0,0 +1,12 @@
+// https://github.com/kuychaco/algoClass/blob/master/data-structures/binarySearchTree.js
+
+// Abstract data type
+
+// A binary search tree is a tree with the additional constraints:
+// - each node has only two child nodes (node.left and node.right)
+// - all the values in the left subtree of a node are
+//     less than or equal to the value of the node
+// - all the values in the right subtree of a node 
+//     are greater than the value of the node
+
+
