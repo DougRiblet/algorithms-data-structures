2eb64f6c14b5ff404c3ebfd34fe8c5d2d85ad0e6
Doug Riblet <smashzen85@gmail.com>
2017-08-02 16:07:46 -0500

    add BST traverse depth-first methods

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -20,11 +20,91 @@ function BinarySearchTree (value) {
 // EXERCISES: Implement these methods and note time complexity for each
 
 BinarySearchTree.prototype.insert = function(value) {
-  // implement me...
+
+  function recursiveFind(nod,val){
+    if (val === nod.value) {
+      return;
+    } else if (val < nod.value) {
+      if (nod.left === null) {
+        nod.left = new BinarySearchTree(val);
+        return;
+      } else {
+        recursiveFind(nod.left, val);
+      }
+    } else if (val > nod.value) {
+      if (nod.right === null) {
+        nod.right = new BinarySearchTree(val);
+        return;
+      } else {
+        recursiveFind(nod.right, val);
+      }
+
+    }
+  }
+
+  recursiveFind(this, value);
+  return this;
+
 };
-// Time complexity:
+// Time complexity for insert: logarithmic ... O(log(n))
 
 BinarySearchTree.prototype.contains = function(value) {
-  // implement me...
+  let answer = false;
+
+  function recursiveFind(nod,val){
+    if (val === nod.value) {
+      answer = true;
+      return;
+    } else if (val < nod.value && nod.left) {
+      recursiveFind(nod.left, val); 
+    } else if (val > nod.value && nod.right) {
+      recursiveFind(nod.right, val);
+    }
+  }
+  
+  recursiveFind(this, value);
+  return answer;
 };
-// Time complexity:
+// Time complexity for contains: logarithmic ... O(log(n))
+
+BinarySearchTree.prototype.traverseDepthFirst_inOrder = function(fn) {
+  if (this.left === null && this.right === null){
+    return fn(this);
+  }
+  if (this.left !== null){
+    this.left.traverseDepthFirst_inOrder(fn);
+  }
+  fn(this);
+  if (this.right !== null){
+    this.right.traverseDepthFirst_inOrder(fn);
+  }
+};
+// Time complexity for traverseDepthFirst_inOrder: linear ... O(n)
+
+BinarySearchTree.prototype.traverseDepthFirst_preOrder = function(fn) {
+  if (this.left === null && this.right === null){
+    return fn(this);
+  }
+  fn(this);
+  if (this.left !== null){
+    this.left.traverseDepthFirst_preOrder(fn);
+  }
+  if (this.right !== null){
+    this.right.traverseDepthFirst_preOrder(fn);
+  }
+};
+// Time complexity for traverseDepthFirst_preOrder: linear ... O(n)
+
+BinarySearchTree.prototype.traverseDepthFirst_postOrder = function(fn) {
+  if (this.left === null && this.right === null){
+    return fn(this);
+  }
+  if (this.left !== null){
+    this.left.traverseDepthFirst_postOrder(fn);
+  }
+  if (this.right !== null){
+    this.right.traverseDepthFirst_postOrder(fn);
+  }
+  fn(this);
+};
+// Time complexity for traverseDepthFirst_postOrder: linear ... O(n)
