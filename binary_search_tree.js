f9f592f9be93e40e10702d2ad8f6bfef5525abb1
Doug Riblet <smashzen85@gmail.com>
2017-08-03 12:11:23 -0500

    add BST method checkIfBalanced

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -43,8 +43,8 @@ BinarySearchTree.prototype.insert = function(value) {
   }
 
   recursiveFind(this, value);
-  return this;
 
+  return this;
 };
 // Time complexity for insert: logarithmic ... O(log(n))
 
@@ -67,6 +67,11 @@ BinarySearchTree.prototype.contains = function(value) {
 };
 // Time complexity for contains: logarithmic ... O(log(n))
 
+// METHODS: Three types of BST traversal
+// (1) In-Order traversal is most common
+// Visit left branch, then current node, than right branch
+// For BST, this visits nodes in ascending order (hence the name)
+
 BinarySearchTree.prototype.traverseDepthFirst_inOrder = function(fn) {
   if (this.left === null && this.right === null){
     return fn(this);
@@ -80,6 +85,8 @@ BinarySearchTree.prototype.traverseDepthFirst_inOrder = function(fn) {
   }
 };
 // Time complexity for traverseDepthFirst_inOrder: linear ... O(n)
+
+// (2) Pre-Order traversal visits current node before its child nodes
 
 BinarySearchTree.prototype.traverseDepthFirst_preOrder = function(fn) {
   if (this.left === null && this.right === null){
@@ -95,6 +102,8 @@ BinarySearchTree.prototype.traverseDepthFirst_preOrder = function(fn) {
 };
 // Time complexity for traverseDepthFirst_preOrder: linear ... O(n)
 
+// (3) Post-Order traversal visits the current node after its child nodes
+
 BinarySearchTree.prototype.traverseDepthFirst_postOrder = function(fn) {
   if (this.left === null && this.right === null){
     return fn(this);
@@ -109,6 +118,10 @@ BinarySearchTree.prototype.traverseDepthFirst_postOrder = function(fn) {
 };
 // Time complexity for traverseDepthFirst_postOrder: linear ... O(n)
 
+// METHOD: checkIfFull
+// A binary tree is full if every node has either 
+// zero or two children (no nodes have only one child)
+
 BinarySearchTree.prototype.checkIfFull = function() {
   let answer = true;
   this.traverseDepthFirst_inOrder(function(nod){
@@ -118,7 +131,30 @@ BinarySearchTree.prototype.checkIfFull = function() {
 };
 // Time complexity for checkIfFull: linear ... O(n)
 
+// METHOD: checkIfBalanced
+// For this exercise, let's say that a tree is balanced if the minimum height
+// and the maximum height differ by no more than 1. The height for a branch 
+// is the number of levels below the root.
+
 BinarySearchTree.prototype.checkIfBalanced = function() {
-  // implement me...
+  let leafDepths = [];
+
+  function recursiveDepthCheck(nod,dep){
+    if (!nod.left && !nod.right) {
+      leafDepths.push(dep);
+      return;
+    } else {
+      if (nod.left) {
+        recursiveDepthCheck(nod.left, dep+1); 
+      }
+      if (nod.right) {
+        recursiveDepthCheck(nod.right, dep+1); 
+      }
+    }
+  }
+
+  recursiveDepthCheck(this, 0);
+
+  return Math.max(...leafDepths) - Math.min(...leafDepths) <= 1;
 };
-// Time complexity:
+// Time complexity for checkIfBalanced: linear ... O(n)
