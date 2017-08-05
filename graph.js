bcb547c4ee55023d35d4128373c0e7e9390825ca
Doug Riblet <smashzen85@gmail.com>
2017-08-05 17:35:29 -0500

    add more graph methods

--- graph.js
+++ graph.js
@@ -21,7 +21,7 @@ function Graph () {
 Graph.prototype.addNode = function(value) {
   this._nodes[value] = this._nodes[value] || [];
 };
-// Time complexity:
+// Time complexity for addNode: constant ... O(1)
 
 Graph.prototype.addEdge = function(value1, value2) {
   if (this._nodes[value1] && this._nodes[value2]) {
@@ -30,6 +30,30 @@ Graph.prototype.addEdge = function(value1, value2) {
   }
 
 };
-// Time complexity:
+// Time complexity for addEdge: constant ... O(1)
 
+Graph.prototype.removeNode = function(value) {
+  delete this._nodes[value];
+  for (let key in this._nodes){
+    this._nodes[key] = this._nodes[key].filter(x=>x!==value);
+  }
+};
+// Time complexity for removeNode: quadratic ... O(n^2)
 
+Graph.prototype.contains = function(value) {
+  return this._nodes.hasOwnProperty(value);
+};
+// Time complexity for contains: constant ... O(1)
+
+Graph.prototype.removeEdge = function(value1, value2) {
+  if (this._nodes[value1] && this._nodes[value2]) {
+    this._nodes[value1] = this._nodes[value1].filter(x=>x!==value2);
+    this._nodes[value2] = this._nodes[value2].filter(x=>x!==value1);
+  }
+};
+// Time complexity for removeEdge: linear ... O(n)
+
+Graph.prototype.hasEdge = function(value1, value2) {
+  return this._nodes[value1].includes(value2);
+};
+// Time complexity for hasEdge: linear ... O(n)
