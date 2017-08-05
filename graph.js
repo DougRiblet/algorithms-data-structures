d6036031cb9180e9867553a17471a876a1525501
Doug Riblet <smashzen85@gmail.com>
2017-08-05 11:14:09 -0500

    add graph.js

---
+++ graph.js
@@ -0,0 +1,13 @@
+// https://github.com/kuychaco/algoClass/blob/master/data-structures/graph.js
+
+// GRAPHS
+
+// Abstract data type
+
+// Basic Graph:
+// Stores nodes (represented by any primitive value) and the neighbors for
+// each node. This implementation represents a graph as an adjacency list.
+
+// Constraints:
+// This graph implementation is undirected and can have unconnected nodes.
+// The nodes are represented by unique primitive values.
