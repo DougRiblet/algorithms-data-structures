b5a44474dfb1ee134bcc6567e7f2d28f2431d5b0
Doug Riblet <smashzen85@gmail.com>
2017-08-05 11:57:12 -0500

    add graph constructor and first two methods

--- graph.js
+++ graph.js
@@ -1,7 +1,5 @@
 // https://github.com/kuychaco/algoClass/blob/master/data-structures/graph.js
 
-// GRAPHS
-
 // Abstract data type
 
 // Basic Graph:
@@ -11,3 +9,27 @@
 // Constraints:
 // This graph implementation is undirected and can have unconnected nodes.
 // The nodes are represented by unique primitive values.
+
+// Constructor already provided:
+
+function Graph () {
+  this._nodes = {};
+}
+
+// EXERCISE: Implement these methods
+
+Graph.prototype.addNode = function(value) {
+  this._nodes[value] = this._nodes[value] || [];
+};
+// Time complexity:
+
+Graph.prototype.addEdge = function(value1, value2) {
+  if (this._nodes[value1] && this._nodes[value2]) {
+    this._nodes[value1].push(value2);
+    this._nodes[value2].push(value1);
+  }
+
+};
+// Time complexity:
+
+
