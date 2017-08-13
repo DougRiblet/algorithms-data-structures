2234bae89f5af710fe59f234b530c4826b417b5b
Doug Riblet <smashzen85@gmail.com>
2017-08-13 09:12:04 -0500

    tweak selection sort, ready to test

--- shell_sort.js
+++ shell_sort.js
@@ -2,9 +2,28 @@
 function shellSort(arr){
   let seq = generateSequence(arr.length);
 
+  while(seq.length > 0){
+    let v = seq.pop();
+
+    for (let i=0; i<v; i++){
+      
+
+
+
+      
+    }
+
+  }
+
+
+
 }
 
-
+function swap(arr, j, k){
+  let temp = arr[j];
+  arr[j] = arr[k];
+  arr[k] = temp;
+}
 
 function generateSequence(num){
   let pool = [];
