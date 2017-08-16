fa543d21cc0d9e34bd8a8e8cd3f7b42bcd4d639a
Doug Riblet <smashzen85@gmail.com>
2017-08-16 13:21:44 -0500

    write function for cocktail sort

--- cocktail_sort.js
+++ cocktail_sort.js
@@ -5,7 +5,38 @@
 // Implement cocktail sort (for each pass find both min and max values
 //   and sort in both directions). How does this impact performance?
 
-
+function cocktailSort(input){
+  let cLo = 0, cHi = input.length-1, pass = 1;
+  while (cLo < cHi){
+    // odd-numbered pass goes left to right
+    // even-numbered pass goes right to left
+    if (pass % 2 === 1){
+      let newcHi = cLo;
+      for (let x=cLo; x<cHi; x++){
+        if (input[x] > input[x+1]) {
+          let temp = input[x];
+          input[x] = input[x+1];
+          input[x+1] = temp;
+          newcHi = x;
+        }
+      }
+      cHi = newcHi;
+    } else if (pass % 2 === 0){
+      let newcLo = cHi;
+      for (let y=cHi; y>cLo; y--){
+        if (input[y] < input[y-1]){
+          let temp = input[y];
+          input[y] = input[y-1];
+          input[y-1] = temp;
+          newcLo = y;          
+        }
+      }
+      cLo = newcLo;
+    }
+    pass++;
+  }
+  return input;
+}
 
 // function bubbleSort (input) {
 //   let cutoff = input.length-1;
